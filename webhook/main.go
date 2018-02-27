24a68bf4012e0ee18761644f3566ca70e712e90f
Ryota Arai <ryota.arai@gmail.com>
2018-02-28 00:32:37 +0900

    Update webhook.

--- webhook/main.go
+++ webhook/main.go
@@ -1,14 +1,33 @@
 package main
 
 import (
+	"encoding/json"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
+
+	"k8s.io/api/admission/v1beta1"
 )
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		body, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			panic(err)
+		}
+
 		log.Printf("%+v", r)
+		log.Printf("request body: %s", body)
+
+		response := v1beta1.AdmissionReview{
+			Response: &v1beta1.AdmissionResponse{
+				Allowed: true,
+			},
+		}
+
+		resp, err := json.Marshal(response)
+		w.Write(resp)
 	})
 
 	listen := ":8080"
